dbe4a9cea8c31dd95ab9a57df21312678e071e3e
pratikpatelx <pratikpatel2369@outlook.com>
2021-05-04 15:41:22 +0300

    Implemented Bubble Sort

--- sorting-app/src/SortingAlgorithms/BubbleSort.js
+++ sorting-app/src/SortingAlgorithms/BubbleSort.js
@@ -1,28 +1,35 @@
+/*
+Author: Pratik Patel
+Purpose: BubbleSort.js contains the implementation of the BubbleSort algorithm 
+*/
+import { swapElements } from "./InsertionSort";
+
+/* The purpose of this function is that it does the bubble sort implementation by calling an helper method
+which does the actual bubble sorting implementation */
 export function BubbleSort(array) {
+  //the animations array
   const animations = [];
+  //helper method that does the bubble sort
   bubblesortHelper(array, animations);
   return animations;
 }
-
+/* This function does the actual comparison based sorting using the bubble sort algorithm*/
 function bubblesortHelper(array, animations) {
+  //the last position of the array
   const lastSorted = array.length - 1;
   for (let i = 0; i < lastSorted; i++) {
     for (let j = 0; j < lastSorted - i; j++) {
       animations.push([[j, j + 1], false]);
       animations.push([[j, j + 1], false]);
+      //check if value is greater than the other value in the array, if it is then swap
       if (array[j] > array[j + 1]) {
         animations.push([[j, array[j + 1]], true]);
         animations.push([[j + 1, array[j]], true]);
+        //swap the values
         swapElements(array, j, j + 1);
       }
     }
   }
-}
-
-function swapElements(array, index, tempIndex) {
-  var temp = array[index];
-  array[index] = array[tempIndex];
-  array[tempIndex] = temp;
 }
 
 export default BubbleSort;
